633998d96ab60ffa8e0b433dbd0a91d86ba83cee
Taylan Tatlı <taylantatli90@gmail.com>
2026-05-06 19:39:26 +0300

    chore: keep only apps, status and places icons
    Fallback to inherited themes for all other icon types.

--- install.sh
+++ install.sh
@@ -65,14 +65,14 @@ install() {
 
   if [[ ${color} == '' ]]; then
     mkdir -p                                                                               ${THEME_DIR}/status
-    cp -r ${SRC_DIR}/src/{actions,animations,apps,categories,devices,emblems,mimes,places} ${THEME_DIR}
+    cp -r ${SRC_DIR}/src/{apps,places} ${THEME_DIR}
     cp -r ${SRC_DIR}/src/status/{16,22,24,32,symbolic}                                     ${THEME_DIR}/status
 
     if [[ ${white} == 'true' ]]; then
       sed -i "s/#222222/#ffffff/g" "${THEME_DIR}"/status/{16,22,24}/*
     fi
 
-    cp -r ${SRC_DIR}/links/{actions,apps,categories,devices,emblems,mimes,places,status} ${THEME_DIR}
+    cp -r ${SRC_DIR}/links/{apps,places,status} ${THEME_DIR}
   fi
 
   if [[ ${color} == '' && ${theme} != '' ]]; then
@@ -85,50 +85,28 @@ install() {
   fi
 
   if [[ ${color} == '-dark' ]]; then
-    mkdir -p                                                                           ${THEME_DIR}/{apps,categories,emblems,devices,mimes,places,status}
+    mkdir -p                                                                           ${THEME_DIR}/{apps,places,status}
 
-    cp -r ${SRC_DIR}/src/actions                                                       ${THEME_DIR}
     cp -r ${SRC_DIR}/src/apps/symbolic                                                 ${THEME_DIR}/apps
-    cp -r ${SRC_DIR}/src/categories/symbolic                                           ${THEME_DIR}/categories
-    cp -r ${SRC_DIR}/src/emblems/symbolic                                              ${THEME_DIR}/emblems
-    cp -r ${SRC_DIR}/src/mimes/symbolic                                                ${THEME_DIR}/mimes
-    cp -r ${SRC_DIR}/src/devices/{16,22,24,symbolic}                                   ${THEME_DIR}/devices
     cp -r ${SRC_DIR}/src/places/{16,22,24,symbolic}                                    ${THEME_DIR}/places
     cp -r ${SRC_DIR}/src/status/{16,22,24,symbolic}                                    ${THEME_DIR}/status
 
     # Change icon color for dark theme
-    sed -i "s/#222222/#ffffff/g" "${THEME_DIR}"/{actions,devices,places,status}/{16,22,24}/*
-    sed -i "s/#222222/#ffffff/g" "${THEME_DIR}"/{actions,apps,categories,emblems,devices,mimes,places,status}/symbolic/*
+    sed -i "s/#222222/#ffffff/g" "${THEME_DIR}"/status/{16,22,24}/*
+    sed -i "s/#222222/#ffffff/g" "${THEME_DIR}"/{apps,places,status}/symbolic/*
 
-    cp -r ${SRC_DIR}/links/actions/{16,22,24,symbolic}                                 ${THEME_DIR}/actions
-    cp -r ${SRC_DIR}/links/devices/{16,22,24,symbolic}                                 ${THEME_DIR}/devices
     cp -r ${SRC_DIR}/links/places/{16,22,24,symbolic}                                  ${THEME_DIR}/places
     cp -r ${SRC_DIR}/links/status/{16,22,24,symbolic}                                  ${THEME_DIR}/status
     cp -r ${SRC_DIR}/links/apps/symbolic                                               ${THEME_DIR}/apps
-    cp -r ${SRC_DIR}/links/categories/symbolic                                         ${THEME_DIR}/categories
-    cp -r ${SRC_DIR}/links/mimes/symbolic                                              ${THEME_DIR}/mimes
 
     cd ${dest}
-    ln -s ../${name}${theme}/animations ${name}${theme}-dark/animations
-    ln -s ../../${name}${theme}/categories/32 ${name}${theme}-dark/categories/32
-    ln -s ../../${name}${theme}/emblems/16 ${name}${theme}-dark/emblems/16
-    ln -s ../../${name}${theme}/emblems/22 ${name}${theme}-dark/emblems/22
-    ln -s ../../${name}${theme}/emblems/24 ${name}${theme}-dark/emblems/24
-    ln -s ../../${name}${theme}/mimes/48 ${name}${theme}-dark/mimes/48
     ln -s ../../${name}${theme}/apps/scalable ${name}${theme}-dark/apps/scalable
-    ln -s ../../${name}${theme}/devices/scalable ${name}${theme}-dark/devices/scalable
     ln -s ../../${name}${theme}/places/48 ${name}${theme}-dark/places/48
     ln -s ../../${name}${theme}/status/32 ${name}${theme}-dark/status/32
   fi
 
   cd ${THEME_DIR}
-  ln -sf actions actions@2x
-  ln -sf animations animations@2x
   ln -sf apps apps@2x
-  ln -sf categories categories@2x
-  ln -sf devices devices@2x
-  ln -sf emblems emblems@2x
-  ln -sf mimes mimes@2x
   ln -sf places places@2x
   ln -sf status status@2x
 
